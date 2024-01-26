3938fad920e761feabbf13e970363091cd9f8d9b
franconil <franconil04@gmail.com>
2024-01-27 00:10:30 +0100

    commit primero

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,40 @@
+class Rectangulo(base : Int,altura:Int){
+    private var base = 0
+        set(value) {
+            require(value > 0) {"base necesita ser mayor a 0"}
+            field = value
+        }
+    init {
+        this.base = base
+    }
+
+    private var altura = 0
+        set(value) {
+            require(value > 0){"La altura tiene que ser mayor a 0"}
+            field = value
+        }
+    init {
+        this.altura = altura
+    }
+
+    private var area = base * altura
+
+    private var perimetro = (base * 2) + (altura * 2)
+
+    override fun toString(): String {
+        return "El area es ${this.area} y el perimetro es ${this.perimetro}"
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    try {
+        val rectangulo1 = Rectangulo(0, 20)
+        println(rectangulo1.toString())
+
+        val rectangulo2 = Rectangulo(15, 2230)
+        println(rectangulo2.toString())
+
+    }catch (e: IllegalArgumentException){
+        println(e.message)
+    }
 }
